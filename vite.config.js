d8216ebbe81a6f2024e88bcb53c69e19fef0a268
Max Pöschl <poeschl.max@gmail.com>
2023-12-23 03:48:24 +0100

    Fix composer.json, add js files to vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/grossCalculator.js',
+                'resources/js/toggleDepreciation.js',
             ],
             refresh: true,
         }),
