8b2650ec4c54649b0dbb569c28ef90489e44470f
Max Pöschl <poeschl.max@gmail.com>
2023-12-29 19:26:08 +0100

    Try to make tests work in ci

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -20,6 +20,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
+        $this->withoutExceptionHandling();
         $user = User::factory()->create();
 
         $response = $this->post('/login', [
@@ -33,6 +34,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
+        $this->withoutExceptionHandling();
         $user = User::factory()->create();
 
         $this->post('/login', [
@@ -45,6 +47,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_logout(): void
     {
+        $this->withoutExceptionHandling();
         $user = User::factory()->create();
 
         $response = $this->actingAs($user)->post('/logout');
