5a0b36b8d952f06cb31c344680d6cffe5572f321
Max Pöschl <poeschl.max@gmail.com>
2023-12-30 01:52:06 +0100

    Rename ExampleTest to DashboardTest

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -7,16 +7,16 @@ namespace Tests\Feature;
 use App\Models\User;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class DashboardTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_a_guest_user_should_get_redirected_to_login(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(302);
+        $response->assertRedirect('/login');
     }
 
     public function test_an_action_that_requires_authentication(): void
