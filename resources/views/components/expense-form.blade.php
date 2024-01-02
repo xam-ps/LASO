3b5f32bd298659a609312251d0343c98d3532326
Max Pöschl <poeschl.max@gmail.com>
2024-01-02 16:36:13 +0100

    Add input validation to expense form and fix bug in select field

--- resources/views/components/expense-form.blade.php
+++ resources/views/components/expense-form.blade.php
@@ -8,14 +8,23 @@
         @endif
         @csrf
         <label for="billing_date">Rechnungsdatum:</label><br>
+        @error('billing_date')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input type="date" id="billing_date" name="billing_date"
             value="{{ old('billing_date', $expense->billing_date ?? $now) }}"><br>
 
         <label for="payment_date">Zahlungseingang:</label><br>
+        @error('payment_date')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input type="date" id="payment_date" name="payment_date"
             value="{{ old('payment_date', $expense->payment_date ?? $now) }}"><br>
 
         <label for="supplier_name">Lieferant:</label><br>
+        @error('supplier_name')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="supplier_name" name="supplier_name" list="suppliers"
             value="{{ old('supplier_name', $expense->supplier_name ?? '') }}"><br>
         <datalist id="suppliers">
@@ -25,24 +34,39 @@
         </datalist>
 
         <label for="product_name">Produkt:</label><br>
+        @error('product_name')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="product_name" name="product_name"
             value="{{ old('product_name', $expense->product_name ?? '') }}"><br>
 
         <label for="invoice_number">Rechnungsnummer:</label><br>
+        @error('invoice_number')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="invoice_number" name="invoice_number"
             value="{{ old('invoice_number', $expense->invoice_number ?? '') }}"><br>
 
         <label for="net">Netto:</label><br>
+        @error('net')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="net" name="net" type="number" value="{{ old('net', $expense->net ?? '') }}" min="0" step="0.01">
         €<br>
 
         <label for="tax">Steuer:</label><br>
+        @error('tax')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="tax" name="tax" type="number" value="{{ old('tax', $expense->tax ?? '') }}" min="0" step="0.01"
             style="width: 148px"> €
         <input id="tax_rate" type="number" min="0" max="30" value="19" style="width: 78px"> %
         <br>
 
         <label for="gross">Brutto:</label><br>
+        @error('gross')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <input id="gross" name="gross" type="number" value="{{ old('gross', $expense->gross ?? '') }}" min="0"
             step="0.01">
         €<br>
@@ -51,9 +75,16 @@
         <span id="costTypeInfo"
             class="cursor-pointer mb-2 ml-1 border-black dark:border-gray-200 inline-block rounded-full leading-none"
             style="padding: 1px 8px; border-width: 1px" tabindex="0">i</span><br>
+        @error('cost_type')
+        <div class="alert">{{ $message }}</div>
+        @enderror
         <select name="cost_type" id="cost_type">
             @foreach ($cost_types as $cost_type)
-            <option @if (isset($expense) && ($cost_type->id == $expense->cost_type_id)) selected @endif
+            @if (isset($expense))
+            <option @if ($cost_type->id == $expense->cost_type_id) selected @endif
+                @else
+            <option @if ($cost_type->id == old('cost_type')) selected @endif
+                @endif
                 style="background: #{{$cost_type->color_code}}" value="{{$cost_type->id}}">
                 {{$cost_type->short_name}}
             </option>
@@ -63,19 +94,12 @@
 
         <div class="hidden">
             <label for="depreciation">Abschreibedauer:</label><br>
+            @error('depreciation')
+            <div class="alert">{{ $message }}</div>
+            @enderror
             <input id="depreciation" name="depreciation" type="number"
                 value="{{ old('depreciation', $expense->depreciation ?? '') }}" min="0" max="30" step="1">
         </div>
-
-        @if ($errors->any())
-        <div class="text-red-600">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
 
         <div class="text-center">
             <x-primary-button class="mt-4">
