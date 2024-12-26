6e592cd15a6e3f2c0491c4d0b26a952c0746109d
Max Pöschl <poeschl.max@gmail.com>
2024-12-26 16:35:48 +0100

    Modify the revenue and expense factories to make sure that payment date is always after billing date

--- database/factories/RevenueFactory.php
+++ database/factories/RevenueFactory.php
@@ -20,9 +20,11 @@ class RevenueFactory extends Factory
         $tax = (int) $net * env('DEFAULT_TAX_RATE') / 100;
         $gross = $net + $tax;
 
+        $billigDate = $this->faker->dateTimeThisYear();
+
         return [
-            'billing_date' => $this->faker->dateTimeThisYear(),
-            'payment_date' => $this->faker->dateTimeThisYear(),
+            'billing_date' => $billigDate,
+            'payment_date' => $this->getPaymentDateFromBillingDate($billigDate),
             'company_name' => $this->faker->company,
             'invoice_number' => $this->faker->randomNumber(8),
             'net' => $net,
@@ -36,9 +38,17 @@ class RevenueFactory extends Factory
         return $this->state(function (array $attributes) use ($yearsBack) {
             $revenue = $this->definition();
             $revenue['billing_date'] = $this->faker->dateTimeBetween('-'.$yearsBack.' years', '-'.$yearsBack.' years');
-            $revenue['payment_date'] = $this->faker->dateTimeBetween('-'.$yearsBack.' years', '-'.$yearsBack.' years');
+            $revenue['payment_date'] = $this->getPaymentDateFromBillingDate($revenue['billing_date']);
 
             return $revenue;
         });
+    }
+
+    private function getPaymentDateFromBillingDate($billingDate)
+    {
+        $maxPaymentDate = (clone $billingDate)->modify('+14 days');
+        $endOfYear = (clone $billingDate)->setDate($billingDate->format('Y'), 12, 31);
+
+        return $this->faker->dateTimeBetween($billingDate, min($maxPaymentDate, $endOfYear));
     }
 }
